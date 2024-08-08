24dd4ab60469c2ca11a784783e5e19c840f07fbb
mohammadpy78 <mboroushak@yahoo.com>
2024-08-08 14:45:15 +0330

    middleware
    set def middleware

--- core/main.py
+++ core/main.py
@@ -1,19 +1,43 @@
 import telebot
+from telebot import apihelper
 import os
 import json
+from datetime import datetime
+
+
+c = datetime.now()
+
+
+apihelper.ENABLE_MIDDLEWARE = True
 
 API_TOKEN =os.environ.get('API_TOKEN') 
 bot = telebot.TeleBot(API_TOKEN)
 
+
 # Handle '/start' and '/help'
-# @bot.message_handler(commands=['help', 'start'])
-# def send_welcome(message):
-#     bot.reply_to(message, """\
-# Hi there, I am pybot.
-# I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-# """)
-    #   bot.send_message(message.chat.id,json.dumps(message.chat.__dict__,indent=4,ensure_ascii=False))
-      # bot.reply_to(message,'hi there')
+@bot.message_handler(commands=['help', 'start'])
+def send_welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am pybot.
+I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
+""")
+
+
+@bot.middleware_handler(update_types=['message'])
+def modify_message(bot_instance, message):
+    # modifying the message before it reaches any other handler 
+    message.another_text = message.text + ':changed'
+    
+
+
+@bot.message_handler(func=lambda message : True)
+def handle_text_doc(message):
+    print(message.another_text,c)
+	# bot.reply_to(message,message.another_text)
+    
+    
+     
+
 
 # @bot.message_handler(content_types=['document', 'audio' ])
 # def handel_doc_aud(message):
@@ -24,9 +48,7 @@ bot = telebot.TeleBot(API_TOKEN)
 # @bot.message_handler(regexp="amin")
 # def handle_message(message):
 # 	print('sayed hi')
-@bot.message_handler(func=lambda message: message.text == 'Hi')
-def handle_text_doc(message):
-	print('lambda')
+
 
 
 
